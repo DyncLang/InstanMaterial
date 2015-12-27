0232e85e0eab1c75158e20aa4352fe61ead7a0b5
langlixiaobailong <zhulang@bync.cc>
2015-12-27 16:17:34 +0800

    Initial commit

---
+++ app/src/main/java/cn/zlpro/cn/instanmaterial/acitvity/MainActivity.java
@@ -0,0 +1,127 @@
+package cn.zlpro.cn.instanmaterial.acitvity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.OvershootInterpolator;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.zlpro.cn.instanmaterial.R;
+import cn.zlpro.cn.instanmaterial.adapter.FeedAdapter;
+import cn.zlpro.cn.instanmaterial.utli.Utils;
+
+public class MainActivity extends AppCompatActivity
+{
+    private MenuItem inboxMenuItem;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.rvFeed)
+    RecyclerView rvFeed;
+    @Bind(R.id.btnCreate)
+    ImageButton btnCreate;
+    @Bind(R.id.ivLogo)
+    ImageView ivLogo;
+
+    private FeedAdapter feedAdapter;
+
+    private boolean pendingIntroAnimation;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        if (savedInstanceState == null) {
+            pendingIntroAnimation = true;
+        }
+        setupToolbar();
+        setupFeed();
+
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
+        //设置toolber左边的图片
+        toolbar.setNavigationIcon(R.drawable.ic_menu_white);
+    }
+
+    private void setupFeed() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvFeed.setLayoutManager(linearLayoutManager);
+        feedAdapter = new FeedAdapter(this);
+        rvFeed.setAdapter(feedAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        inboxMenuItem = menu.findItem(R.id.action_inbox);
+        inboxMenuItem.setActionView(R.layout.menu_item_view);
+        if (pendingIntroAnimation) {
+            pendingIntroAnimation = false;
+            startIntroAnimation();
+        }
+        return true;
+    }
+
+
+    private long ANIM_DURATION_TOOLBAR = 300;
+    private void startIntroAnimation() {
+
+        //出现动画的逻辑：
+        //1.android　onCreate的时候我们是看不到界面的，所以我们可以在这里设置动画。
+        //2.在菜单创建并且执行结束以后在执行我们目前可以看到的其他动画。
+        //3.设置时间差。这样才会有不断出现的效果。
+        btnCreate.setTranslationY(2 * getResources().getDimensionPixelOffset(R.dimen.btn_fab_size));
+        int actionbarSize = Utils.dpToPx(56);
+
+        toolbar.setTranslationY(-actionbarSize);
+        ivLogo.setTranslationY(-actionbarSize);
+        inboxMenuItem.getActionView().setTranslationY(-actionbarSize);
+        toolbar.animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_TOOLBAR)
+                .setStartDelay(300).start();
+        ivLogo.animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_TOOLBAR)
+                .setStartDelay(400).start();
+        inboxMenuItem.getActionView().animate()
+                .translationY(0)
+                .setDuration(ANIM_DURATION_TOOLBAR)
+                .setStartDelay(500)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        startContentAnimation();
+                    }
+                })
+                .start();
+    }
+
+    private static final int ANIM_DURATION_FAB = 400;
+
+    private void startContentAnimation() {
+        btnCreate.animate()
+                .translationY(0)
+                .setInterpolator(new OvershootInterpolator(1.f))
+                .setStartDelay(300)
+                .setDuration(ANIM_DURATION_FAB)
+                .start();
+        feedAdapter.updateItems();
+    }
+}
---
+++ app/src/main/java/cn/zlpro/cn/instanmaterial/adapter/FeedAdapter.java
@@ -0,0 +1,92 @@
+package cn.zlpro.cn.instanmaterial.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.zlpro.cn.instanmaterial.R;
+import cn.zlpro.cn.instanmaterial.utli.Utils;
+import cn.zlpro.cn.instanmaterial.view.SquaredImageView;
+
+/**
+ * Created by 小白龙 on 2015/12/27 0027.
+ */
+public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+{
+    private static final int ANIMATED_ITEMS_COUNT = 2;
+
+    private Context context;
+    private int lastAnimatedPosition = -1;
+    private int itemsCount = 0;
+    public FeedAdapter(Context context)
+    {
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        final View view = LayoutInflater.from(context).inflate(R.layout.item_feed, parent, false);
+        return new CellFeedViewHolder(view);
+    }
+
+    private void runEnterAnimation(View view, int position) {
+        if (position >= ANIMATED_ITEMS_COUNT - 1) {
+            return;
+        }
+        if (position > lastAnimatedPosition) {
+            lastAnimatedPosition = position;
+            view.setTranslationY(Utils.getScreenHeight(context));
+            view.animate()
+                    .translationY(0)
+                    .setInterpolator(new DecelerateInterpolator(3.f))
+                    .setDuration(700)
+                    .start();
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+    {
+
+        //这个方法用来设置我们的内容
+        //这里的itemView就是我们设置的根布局的View
+        runEnterAnimation(viewHolder.itemView, position);
+        CellFeedViewHolder holder = (CellFeedViewHolder) viewHolder;
+        if (position % 2 == 0) {
+            holder.ivFeedCenter.setImageResource(R.drawable.img_feed_center_1);
+            holder.ivFeedBottom.setImageResource(R.drawable.img_feed_bottom_1);
+        } else {
+            holder.ivFeedCenter.setImageResource(R.drawable.img_feed_center_2);
+            holder.ivFeedBottom.setImageResource(R.drawable.img_feed_bottom_2);
+        }
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return 20;
+    }
+
+    public static class CellFeedViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.ivFeedCenter)
+        SquaredImageView ivFeedCenter;
+        @Bind(R.id.ivFeedBottom)
+        ImageView ivFeedBottom;
+        public CellFeedViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    public void updateItems() {
+        itemsCount = 10;
+        notifyDataSetChanged();
+    }
+}
